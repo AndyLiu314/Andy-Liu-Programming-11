5b425e072e677bc7be816490ef10deda291f9348
AndyLiu314 <andyliu4002@gmail.com>
2019-11-27 21:48:51 -0800

    Rock, Paper Scissors Game

---
+++ Rock Paper Scissors/src/com/company/Main.java
@@ -0,0 +1,95 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scan = new Scanner(System.in);
+        //This generates a random number between 0 and 999
+        double random = Math.random() * 1000;
+        /*after a random number is obtained, divide it by 3 and the remainder is either 0, 1, or 2
+        If the integer is 0, the computer chooses rock
+        If the integer is 1, the computer chooses paper
+        If the integer is 2, the computer chooses scissors
+         */
+        int response = (int) random % 3;
+
+
+        //I set string names here so I don't have to type these words out over and over again
+        String uc = "Player Choice: ";
+        String cc = "Computer Choice: ";
+        String s = "Scissors!";
+        String p = "Paper!";
+        String r = "Rock!";
+        String w = "You Win!";
+        String l = "You Lose!";
+        String d = "Draw!";
+
+
+        //This is the opening line and the instructions for the game
+        System.out.println("Choose rock, paper, or scissors. Type 'r' for rock, 'p' for paper, and 's' and for scissors.");
+
+
+        //this scans for user input which equals choice
+        String choice = scan.next();
+
+
+        //if your choice is "r", the console will state "You win!", "You Lost!" or "Draw!" which depends on the number it got.
+        if (choice.equals("r")) {
+            /*after that, it will display it's choice of rock, paper or scissors depending on the randomly selected number
+            It will also show the player's input and what they chose.
+             */
+            if (response == 2) {
+                //this first line displays the outcome of the game and the whether you won or lost
+                System.out.println(w);
+                //the second line displays the computer choice first and then the player choice.
+                System.out.println(cc + s + "\t" + uc + r);
+                //this process is repeated exactly the same way for the three other outcomes
+            } else if (response == 0) {
+                System.out.println(d);
+                System.out.println(cc + r + "\t"+ uc + r);
+            } else if (response == 1) {
+                System.out.println(l);
+                System.out.println(cc + p + "\t"+ uc + r);
+            }
+
+
+        //If your choice is "p", the console will state "You win!", "You Lost!" or "Draw!" which depends on the number it got.
+        } else if (choice.equals("p")) {
+            //this process is the exact same as the one for rock
+            //the only difference is that the results will change because the player picked paper
+            if (response == 2) {
+                System.out.println(l);
+                System.out.println(cc + s + "\t"+ uc + p);
+            } else if (response == 0) {
+                System.out.println(w);
+                System.out.println(cc + r + "\t"+ uc + p);
+            } else if (response == 1) {
+                System.out.println(d);
+                System.out.println(cc + p + "\t"+ uc + p);
+            }
+
+
+            //If your choice is "s", the console will state "You win!", "You Lost!" or "Draw!" which depends on the number it got.
+        } else if (choice.equals("s")) {
+            //the process here is also the same as the other ones mentioned above except the results will be based off the player picking scissors.
+            if (response == 2) {
+                System.out.println(d);
+                System.out.println(cc + s + "\t"+ uc + s);
+            } else if (response == 0) {
+                System.out.println(l);
+                System.out.println(cc + r + "\t"+ uc + s);
+            } else if (response == 1) {
+                System.out.println(w);
+                System.out.println(cc + p + "\t"+ uc + s);
+            }
+
+        //If anything other than 'r', 's', or, 'p' is inputted, an error message will popup telling you to play again
+        } else {
+            System.out.println("Oops, invalid selection, please play again.");
+        }
+    }
+}
+
+
